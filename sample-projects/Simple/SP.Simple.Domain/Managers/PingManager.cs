94c72bf005333e920fc2b3f4158ce015fe2b6515
Aleksa Ristic <aristiccitsira@gmail.com>
2023-12-03 01:33:27 +0100

    Stage

---
+++ sample-projects/Simple/SP.Simple.Domain/Managers/PingManager.cs
@@ -0,0 +1,15 @@
+﻿using LSCore.Contracts.Http;
+using SP.Simple.Contracts.IManagers;
+
+namespace SP.Simple.Domain.Managers
+{
+    public class PingManager : IPingManager
+    {
+        /// <summary>
+        /// Returns new empty response object with default values
+        /// </summary>
+        /// <returns></returns>
+        public LSCoreResponse GetPing() =>
+            new LSCoreResponse();
+    }
+}
