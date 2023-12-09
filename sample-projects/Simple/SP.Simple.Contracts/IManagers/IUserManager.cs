daf675ef78996eb6ac0fd3f88b837851679b1663
Aleksa Ristic <aristiccitsira@gmail.com>
2023-12-09 17:49:41 +0100

    Added login mock to SP.Simple

---
+++ sample-projects/Simple/SP.Simple.Contracts/IManagers/IUserManager.cs
@@ -0,0 +1,12 @@
+﻿using LSCore.Contracts.Http;
+using LSCore.Contracts.IManagers;
+using SP.Simple.Contracts.Requests.Users;
+
+namespace SP.Simple.Contracts.IManagers
+{
+    public interface IUserManager : ILSCoreBaseManager
+    {
+        LSCoreResponse<string> Login(LoginUserRequest request);
+        LSCoreResponse<string> Me();
+    }
+}
---
+++ sample-projects/Simple/SP.Simple.Contracts/Requests/Users/LoginUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SP.Simple.Contracts.Requests.Users
+{
+    public class LoginUserRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
