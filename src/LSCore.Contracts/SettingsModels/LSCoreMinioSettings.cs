f7c6e066c9a241b74c91750742111d40793f4212
Aleksa Milic <aleksamilickv@gmail.com>
2023-12-09 14:48:03 +0100

    Minio credentials fix

---
+++ src/LSCore.Contracts/SettingsModels/LSCoreMinioSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace LSCore.Contracts.SettingsModels
+{
+    public class LSCoreMinioSettings
+    {
+        public string BucketBase;
+        public string Host;
+        public string AccessKey;
+        public string SecretKey;
+        public string Port;
+    }
+}
