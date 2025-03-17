4e6d8b18add5cb3ea4b69e30e6a522923f117cc7
Aleksa Ristic <aristiccitsira@gmail.com>
2025-03-17 17:12:50 -0400

    Fixed ILSCoreAuthUserPassManager naming

--- src/LSCore.Auth/LSCore.Auth.UserPass.Contracts/ILSCoreAuthUserPassManager.cs
+++ src/LSCore.Auth/LSCore.Auth.UserPass.Contracts/ILSCoreAuthUserPassManager.cs
@@ -2,7 +2,7 @@ using LSCore.Auth.Contracts;
 
 namespace LSCore.Auth.UserPass.Contracts;
 
-public interface ILSCoreAuthPasswordManager<in TEntityIdentifier>
+public interface ILSCoreAuthUserPassManager<in TEntityIdentifier>
 {
 	LSCoreJwt Authenticate(TEntityIdentifier identifier, string password);
 }
