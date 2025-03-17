399f6cb8649b6fdc39c900742d5c1ee96c98b792
Aleksa Ristic <aristiccitsira@gmail.com>
2025-03-16 20:55:48 -0400

    Version upgrade to 9.1.0

--- tools/version-upgrade/version-upgrade.js
+++ tools/version-upgrade/version-upgrade.js
@@ -1,82 +1,120 @@
-const fs = require('fs')
-const { XMLParser, XMLBuilder } = require('fast-xml-parser')
+const fs = require('fs');
+const { XMLParser, XMLBuilder } = require('fast-xml-parser');
+const { execFile } = require('child_process');
+const path = require('path');
 
 const xmlOptions = {
-	ignoreAttributes: false,
-	format: true,
-	unpairedTags: ["PackageReference", "ProjectReference"],
-	suppressUnpairedNode: false,
-}
+  ignoreAttributes: false,
+  format: true,
+  unpairedTags: ['PackageReference', 'ProjectReference'],
+  suppressUnpairedNode: false,
+};
 
-var config = {}
+let config = {};
 
-var args = process.argv.slice(2)
+const args = process.argv.slice(2);
 
-fs.exists('./version-upgrade.config', (e) => {
-	if(e) {
-		config = JSON.parse(fs.readFileSync('./version-upgrade.config'))
-	} else {
-		config = {
-			currentVersion: '0.0.1'
-		}
-		fs.writeFileSync('./version-upgrade.config', JSON.stringify(config))
-	}
-	
-	let split = config.currentVersion.split('.')
-	let currentPatch = parseInt(split[2])
-	let currentMinor = parseInt(split[1])
-	
-	// if args contains --upgrade-minor, upgrade minor version
-	if(args.indexOf('--upgrade-minor') >= 0)
-		config.nextVersion = `${split[0]}.${currentMinor + 1}.0`
-	else
-		config.nextVersion = `${split[0]}.${currentMinor}.${currentPatch + 1}`
-	
-	console.log("Config loaded!")
-	console.log(config)
-	console.log()
+const findAllCsProjs = (dir, fileList = []) => {
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
 
-	run()
-})
+  for (let entry of entries) {
+    const fullPath = path.join(dir, entry.name);
+    if (entry.isDirectory()) {
+      findAllCsProjs(fullPath, fileList);
+    } else if (entry.name.endsWith('.csproj')) {
+      fileList.push(fullPath);
+    }
+  }
+  return fileList;
+};
 
+fs.exists('./version-upgrade.config', (exists) => {
+  if (exists) {
+    config = JSON.parse(fs.readFileSync('./version-upgrade.config'));
+  } else {
+    config = {
+      currentVersion: '0.0.1',
+    };
+    fs.writeFileSync('./version-upgrade.config', JSON.stringify(config));
+  }
 
-const run = () => {
+  const split = config.currentVersion.split('.');
+  let currentPatch = parseInt(split[2]);
+  let currentMinor = parseInt(split[1]);
+  let currentMajor = parseInt(split[0]);
 
-	const parser = new XMLParser(xmlOptions)
+  // if args contains --upgrade-minor, upgrade minor version
+  if (args.includes('--upgrade-minor')) {
+    config.nextVersion = `${currentMajor}.${currentMinor + 1}.0`;
+  } else {
+    config.nextVersion = `${currentMajor}.${currentMinor}.${currentPatch + 1}`;
+  }
 
-	const srcPath = '../../src/'
+  console.log('Config loaded!');
+  console.log(config);
+  console.log();
 
-	const projects = fs.readdirSync(srcPath).filter(x => x.indexOf('LSCore') >= 0)
+  const run = () => {
+    const parser = new XMLParser(xmlOptions);
 
-	console.log()
-	console.log("Starting projects versions upgrade...")
+    const srcPath = '../../src/';
 
-	for(var i = 0; i < projects.length; i++) {
-		console.log()
-		console.log(`Updating version on project ${projects[i]}`)
+    const projects = fs
+      .readdirSync(srcPath)
+      .filter((x) => x.indexOf('LSCore') >= 0);
 
-		var projectPath = `${srcPath}${projects[i]}`
-		let csprojFile = fs.readdirSync(`${projectPath}`).find(x => x.indexOf('.csproj') >= 0)
-		let csprojContent = fs.readFileSync(`${projectPath}/${csprojFile}`)
-		let parsed = parser.parse(csprojContent)
+    console.log();
+    console.log('Starting projects versions upgrade...');
 
-		parsed.Project.PropertyGroup.Version = config.nextVersion
+    let allCsProjFiles = [];
+    for (let i = 0; i < projects.length; i++) {
+      const projectPath = path.join(srcPath, projects[i]);
+      const csprojFiles = findAllCsProjs(projectPath);
+      allCsProjFiles = allCsProjFiles.concat(csprojFiles);
+    }
 
-		const builder = new XMLBuilder(xmlOptions);
-		let xmlDataStr = builder.build(parsed);
+    for (let csprojFile of allCsProjFiles) {
+      console.log(`Updating version on project: ${csprojFile}`);
 
-		fs.writeFileSync(`${projectPath}/${csprojFile}`, xmlDataStr)
-	}
+      try {
+        let csprojContent = fs.readFileSync(csprojFile, 'utf-8');
+        let parsed = parser.parse(csprojContent);
 
-	fs.writeFileSync('./version-upgrade.config', JSON.stringify({ currentVersion: config.nextVersion }))
+        // Ensure PropertyGroup exists before trying to modify it
+        if (!parsed.Project.PropertyGroup) {
+          parsed.Project.PropertyGroup = {};
+        }
 
-	const { execFile } = require('child_process');
-	const child = execFile(`./version-upgrade.sh`, [ config.nextVersion ], (error, stdout, stderror) => {
-		if (error) {
-			throw error;
-		}
+        parsed.Project.PropertyGroup.Version = config.nextVersion;
 
-		console.log()
-		console.log(`Version successfully upgraded to ${config.nextVersion}!`)
-	});
-}
+        const builder = new XMLBuilder(xmlOptions);
+        let xmlDataStr = builder.build(parsed);
+
+        fs.writeFileSync(csprojFile, xmlDataStr);
+      } catch (error) {
+        console.error(`Error processing ${csprojFile}:`, error);
+      }
+    }
+
+    fs.writeFileSync(
+      './version-upgrade.config',
+      JSON.stringify({ currentVersion: config.nextVersion })
+    );
+
+    const { execFile } = require('child_process');
+    const child = execFile(
+      `./version-upgrade.sh`,
+      [config.nextVersion],
+      (error, stdout, stderror) => {
+        if (error) {
+          throw error;
+        }
+
+        console.log();
+        console.log(`Version successfully upgraded to ${config.nextVersion}!`);
+      }
+    );
+  };
+
+  run();
+});
