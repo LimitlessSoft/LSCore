e5abb24fbded8bb211461b1a84e88d54d01e2d08
Aleksa Ristic <aristiccitsira@gmail.com>
2025-03-17 18:45:07 -0400

    Added NotAuthenticated property into LSCoreAuthContextEntity<>

--- src/LSCore.Auth/LSCore.Auth.Contracts/LSCoreAuthContextEntity.cs
+++ src/LSCore.Auth/LSCore.Auth.Contracts/LSCoreAuthContextEntity.cs
@@ -2,6 +2,7 @@ namespace LSCore.Auth.Contracts;
 
 public class LSCoreAuthContextEntity<TEntityIdentifier>
 {
+	public bool NotAuthenticated => !IsAuthenticated;
 	public bool IsAuthenticated => Type != null && Identifier != null;
 	public LSCoreAuthEntityType? Type { get; set; }
 	public TEntityIdentifier? Identifier { get; set; }
